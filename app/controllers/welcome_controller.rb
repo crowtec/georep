ab8bf7ffe72011bd283f9c32dc14b80b57e9fc9c
Rodrigo Crespo <rcrespocepeda@gmail.com>
2015-04-18 17:36:43 +0200

    more controller methods

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,31 +1,59 @@
 class WelcomeController < ApplicationController
-
+  include WelcomeHelper
   require 'xmlrpc/client'
 
   def index
     @zones = get_all_zones
   end
 
-  def coordinates_container_zones
-
-    server = XMLRPC::Client.new2("http://178.62.1.193/api")
+  def add_repvalue
     p = {
         authentication:{
             email: "a@b.c",
             user_code: "a0066bbcbe25d7029a0e6934e1aa",
             app_code: "49456203c8e64d3690ed0e0dccf151e4e693"
         },
+        name: "new_reputation_" + (0..10).map { ('a'..'z').to_a[rand(26)] }.join,
+        is_object: true,
+        zone_class: "repvalue",
+        shape:{
+            shape_class: "circular",
+            coordinates:
+                [params[:coordinates]]
+        }
+    }
+    queryCtOS("ctos.createZone", p)
+  end
+
+  def get_location_barrio
+    p = {
+        authentication:{
+            email: "a@b.c",
+            user_code: "a0066bbcbe25d7029a0e6934e1aa",
+            app_code: "49456203c8e64d3690ed0e0dccf151e4e693"
+        },
+        zone_class: "barrio",
         coordinates:
             [params[:coordinates]]
     }
-    resp = server.call("ctos.coordinatesContainerZones", p)
+    queryCtOS("ctos.coordinatesContainerZones", p)
+  end
 
-    # puts params[:coordinates]
-    # render html: CtosRubyClient.hola_unauthenticated
+  def get_location_repvalues
+    p = {
+        authentication:{
+            email: "a@b.c",
+            user_code: "a0066bbcbe25d7029a0e6934e1aa",
+            app_code: "49456203c8e64d3690ed0e0dccf151e4e693"
+        },
+        zone_class: "barrio",
+        coordinates:
+            [params[:coordinates]]
+    }
+    queryCtOS("ctos.coordinatesContainerZones", p)
   end
 
   def get_all_zones
-    server = XMLRPC::Client.new2("http://178.62.1.193/api")
     p = {
         authentication:{
             email: "a@b.c",
@@ -33,7 +61,22 @@ class WelcomeController < ApplicationController
             app_code: "49456203c8e64d3690ed0e0dccf151e4e693"
         }
     }
-    resp = server.call("ctos.getAllZones", p)
+    queryCtOS("ctos.getAllZones", p)
+  end
+
+  def get_all_repvalues
+    p = {
+        authentication:{
+            email: "a@b.c",
+            user_code: "a0066bbcbe25d7029a0e6934e1aa",
+            app_code: "49456203c8e64d3690ed0e0dccf151e4e693"
+        },
+        parameters:{
+            is_object: true,
+            zone_class: "repvalue"
+        }
+    }
+    queryCtOS("ctos.getAllZones", p)
   end
 
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,7 @@
 module WelcomeHelper
+
+  def queryCtOS(method, params)
+    server = XMLRPC::Client.new2("http://178.62.1.193/api")
+    resp = server.call(method, params)
+  end
 end
