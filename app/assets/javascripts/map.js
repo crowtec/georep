714b8a94802921f9e820613f8d3e3e610118f9fe
Meriem <melyamri@gmail.com>
2015-04-18 15:25:11 +0200

    render map

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,80 @@
+
+
+/*$(document).ready(function () {
+ alert("Hello world!");
+ });*/
+
+var rendererOptions = {
+    suppressMarkers : true
+
+    /*markerOptions: {
+     icon: "http://icdn.pro/images/es/c/o/coche-de-transporte-de-ambulancia-de-emergencia-del-vehiculo-icono-6274-96.png"
+     }*/
+};
+
+
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
+function initialize() {
+
+    var mapOptions = {
+        center: new google.maps.LatLng(40.406170, -3.700261),
+        zoom:17
+    };
+    map = new google.maps.Map(document.getElementById('map-container'), mapOptions);
+
+
+
+
+}
+function drawParent(zones){
+    for (key in zones.params.zones) {
+        var geometry = zones.params.zones[key].shape;
+        map.data.loadGeoJson(geometry);
+    }
+
+}
+function drawZones(zones) {
+
+
+    console.log(zones.params.zones);
+    for (key in zones.params.zones) {
+        var geometry = zones.params.zones[key].shape;
+        var coordinates = geometry.coordinates;
+        console.log(geometry);
+        console.log(coordinates);
+
+        var color = '#' + (function co(lor) {
+                return (lor +=
+                    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'][Math.floor(Math.random() * 16)])
+                && (lor.length == 6) ? lor : co(lor);
+            })('');
+
+        var center = new google.maps.LatLng(coordinates[0], coordinates[1]);
+
+        var circleOptions = {
+            strokeColor: color,
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: color,
+            fillOpacity: 0.35,
+            map: map,
+            center: center,
+            radius: 30
+        };
+        // Add the circle for this city to the map.
+        lightCircle = new google.maps.Circle(circleOptions);
+
+        var light = new google.maps.Marker({
+            position: center,
+            map: map,
+            icon: "http://www.basicgo.com/bundles/basicgo/frontend/imagenes/exclamacionAviso.png"
+        });
+
+    }
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    @zones = get_current_zone
+  end
+
+  def get_current_zone
+
   end
 end
