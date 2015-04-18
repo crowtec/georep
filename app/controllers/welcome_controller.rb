ba9d58b642372a7d0594c073f235d412f83664ed
Rodrigo Crespo <rcrespocepeda@gmail.com>
2015-04-18 20:16:27 +0200

    mas cosas

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -14,7 +14,7 @@ class WelcomeController < ApplicationController
         etiquetas: params[:etiquetas],
         valor: params[:valor]
     }
-    coordinate = [40.452666, -3.678407]
+    coordinate = [40.453787, -3.678227]
     p = {
         authentication:{
             email: "a@b.c",
@@ -36,8 +36,49 @@ class WelcomeController < ApplicationController
     }
     queryCtOS("ctos.createZone", p)
 
+
+
+    parent_zone = get_current_zone
+    parent_zone.deep_symbolize_keys!
+    update_barriovalue(parent_zone[:params][:zones][0], data[:valor].to_f)
+
+
     redirect_to root_path
   end
+
+  def update_barriovalue(barrio, valor)
+    barrio_data = eval(barrio[:data_info][:data])
+    if barrio_data[:value]
+      old_value = barrio_data[:value].to_f
+      old_values_count = barrio_data[:count].to_f
+      new_values_count = old_values_count+1
+      new_value = ((old_value * old_values_count) + valor)/(new_values_count)
+      data = {
+          value: new_value,
+          count: new_values_count
+      }
+    else
+      data = {
+          value: valor,
+          count: 1
+      }
+    end
+
+    p = {
+        authentication:{
+            email: "a@b.c",
+            user_code: "a0066bbcbe25d7029a0e6934e1aa",
+            app_code: "49456203c8e64d3690ed0e0dccf151e4e693"
+        },
+        name: barrio[:name],
+        data_info:{
+            data: data
+        }
+
+    }
+    queryCtOS("ctos.updateZone", p)
+  end
+
 
   def get_current_zone
     p = {
@@ -46,11 +87,13 @@ class WelcomeController < ApplicationController
             user_code: "a0066bbcbe25d7029a0e6934e1aa",
             app_code: "49456203c8e64d3690ed0e0dccf151e4e693"
         },
-        zone_class: "barrio",
-        coordinates:
-            [params[:coordinates]]
+        parameters:{
+            is_object: false,
+            zone_class: "barrio",
+            name: "Barrio Hispanoamerica"
+        }
     }
-    queryCtOS("ctos.coordinatesContainerZones", p)
+    queryCtOS("ctos.getZones", p)
   end
 
   def get_current_repvalues
